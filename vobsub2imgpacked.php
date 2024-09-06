801c403f40a131baf9116397410f91c7679777f9
Ludovic Mabilat <gnuk@pasteque>
2024-09-06 06:32:02 +0200

    rename, append name-generator into vob2imgpacked.php directly

--- vobsub2imgpacked.php
+++ vobsub2imgpacked.php
@@ -27,7 +27,62 @@ if (isset($options['o']) && is_dir($options['o'])) {
 // TODO: skip NOTE line
 $lines = 64;
 $columns = 4;
-require 'name-generator.php';
+
+class NameGenerator {
+
+  private $times = 0;
+  public $step = 1;
+  public $loop = 0;
+  public $baseFilename = '';
+  public $filenameExtension = '';
+
+  public function __construct(int $step = 1, string $baseFilename = 'file-', $extension = '') {
+    $this->step = $step;
+    $this->baseFilename = $baseFilename;
+    $this->filenameExtension = $extension;
+  }
+
+  public function current() {
+    return sprintf('%s%d%s', $this->baseFilename, $this->loop + 1, $this->filenameExtension);
+  }
+
+  public function next() {
+    $this->times++;
+    if ($this->times >= $this->step) {
+      $this->loop++;
+      $this->times = 0;
+    }
+    return $this->current();
+  }
+
+  public function totalTimes() {
+    return ($this->loop * $this->step) + $this->times;
+  }
+
+  public function driftReset() {
+    return $this->times ? false : true;
+  }
+
+  public function times() {
+    return $this->times;
+  }
+
+  public function currentPass() {
+    return sprintf('%02d', $this->loop);
+  }
+
+  public function previousPass() {
+    return sprintf('%02d', $this->loop - 1);
+  }
+
+  public function currentRange() {
+    return sprintf('%02d-%02d', $this->loop * $this->step,  $this->totalTimes());
+  }
+
+  public function previousRange() {
+    return sprintf('%02d-%02d', $this->previousPass() * $this->step,  $this->totalTimes() - 1);
+  }
+}
 
 // search for corresponding .vob or .idx files
 $pathInfo = pathinfo($opt_input);
