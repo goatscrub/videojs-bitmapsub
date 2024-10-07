a1c90d4d2120153475ac657fdc4929321806fd4f
goatscrub - Ludovic Mabilat <35750121+goatscrub@users.noreply.github.com>
2024-10-07 04:05:31 +0200

    [wip] vobsub packheader:ok;pespacket in progress

--- vobsub.py
+++ vobsub.py
@@ -57,7 +57,32 @@ if ( header != b'\x00\x00\x01\xba' ):
 #         vobfile_info = vobfileInfo(*[ int(e) for e in vobfile_info.split() ])
 #     vobfile.seek(0)
 #     # TODO: what to do when no info ?
+# file rewind
 vobfile.seek(0)
+
+class Bits:
+    def __init__(self, octets):
+        self.load(octets)
+
+    def load(self, octets):
+        self.seek=0
+        binary_seq_len_format='{{0:0{}b}}'.format(len(octets)*8)
+        self.binary_sequence=binary_seq_len_format.format(int.from_bytes(octets))
+        # print('leno:{} octets:{}'.format(len(octets), octets))
+        # print(int.from_bytes(octets))
+
+    def get(self, nofbits=1):
+        if self.seek > len(self.binary_sequence):
+            # print(self.seek)
+            # print('lenbinseq:{} binseq:{}'.format(len(self.binary_sequence), self.binary_sequence))
+            # print(len(self.binary_sequence))
+            raise Exception('''Out of bound.''')
+        bits=self.binary_sequence[self.seek:self.seek+nofbits]
+        self.seek+=nofbits
+        return int(bits, 2)
+
+    def harvest(self, sequence):
+        return [ self.get(nofbits) for nofbits in sequence ]
 
 def getStreamId(byte):
     if byte == b'\xb9': return 'Program end'
@@ -83,60 +108,183 @@ def getStreamId(byte):
     if byte == b'\xff': return 'Program stream directory'
     return False
 
+def printBinary(octets, packof=32):
+    '''
+    Print bytes input as binary representation.
+    Binary representation is separated by space each 4 bits
+    and a line break is appended after {packof} bits.
+
+    :param octets: binary string
+    :type bytes: byte
+    :param packof: line break after {packof} bits, defaults to 32
+    :type packof: int, optional
+    '''
+    n=0
+    while n<=len(octets):
+        try:
+            print(octets[n:n+4], end=' ')
+            n+=4
+            if not n%64:
+                print()
+        except IndexError:
+            print(octets[n:])
+
+def readSubtitlePacket():
+    ''''''
+
+def readDataPacket():
+    ''''''
+
+def readControlPacket():
+    ''''''
+
+def readPesPacket(octets):
+    ''''''
+    PesPayloadLen=int.from_bytes(octets[4:6])
+    # Extension
+    bits=Bits(octets[6:8])
+    print(
+        '>> {:02b} scrmbl:{:02b} priority:{:b} align:{:b} copyr:{:b} org:{:b}'
+        .format(*bits.harvest([2,2,1,1,1,1]))
+    )
+    PtsDtsFlag, escr, esRate, dsm, copy, crc, extFlag=bits.harvest([2,1,1,1,1,1,1])
+    print(
+        '>> P|DTS flag:{:02b} escr:{:b} esrate:{:b} dsm:{:b} copyInfo:{:b} crc:{:b} extflg:{:b}'
+        .format(PtsDtsFlag, escr, esRate, dsm, copy, crc, extFlag)
+    )
+    PesHeaderDataLen=int.from_bytes(octets[8:9])
+    # test if extra PES header present
+    if (PtsDtsFlag == 0b10):
+        bits.load(octets[9:14])
+        print(bits.harvest([4, 3, 1, 15, 1, 15, 1]))
+    print(f'PES header data length: {PesHeaderDataLen}')
+    print(f'PES payload length: {PesPayloadLen}')
+
+    substreamid=int.from_bytes(vobfile.read(1))
+    print(f'substream id: {substreamid}')
+    PesPayload=vobfile.read(PesPayloadLen-1)
+    print('subtitle packet size: {}'.format(int.from_bytes(PesPayload[:2])))
+    dataPacketSize=int.from_bytes(PesPayload[2:4])
+    print('data packet size: {}'.format(dataPacketSize))
+    print('end sequence position: {}'.format(int.from_bytes(PesPayload[dataPacketSize+2:dataPacketSize+4])))
+    n=0
+    # while n <= 32:
+    #     offset=dataPacketSize+4+n
+    #     print('ctrl sequence: {} {}'.format(dataPacketSize+n, PesPayload[offset:offset+1]))
+    #     n+=1
+
+    # print('ff byte ?: {}'.format(int.from_bytes(PesPayload[dataPacketSize+2:dataPacketSize+4])))
+    # print('ctrl sequence: {}'.format(PesPayload[dataPacketSize+7:dataPacketSize+8]))
+    print()
+
+
+def readPsPacket():
+    ''''''
+
+def readPackHeader(octets):
+    '''
+    Read Pack Header from MPEG-2 Program Stream
+    3 bytes fixed + 1 byte pack identifier
+    14 bytes
+    3 bytes + 1 bytes
+    program mux rate + reserved + pack stuffing length
+    '''
+    identifier=octets[0:4]
+    if identifier != b'\x00\x00\x01\xba':
+        raise Exception ('pouloum, paf.')
+    bits=Bits(octets[4:10])
+    fix, scr3230, w1, scr2915, w2, scr1400, w3, scrext, w4 = bits.harvest([2, 3, 1, 15, 1, 15, 1, 9, 1])
+    # print('fix:{:02b} scr3230:{:03b} w1:{:b} scr2915:{:015b} w2:{:b} scr1400:{:015b} w3:{:b} scrext:{:09b} w4:{:b}'.format(fix, scr3230, w1, scr2915, w2, scr1400, w3, scrext, w4))
+    bits.load(octets[10:14])
+    prg_mux_rate, w5, w6, reserved, stuffing_len = bits.harvest([22, 1, 1, 5, 3])
+    # print('prg_mux_rate:{:022b} w5:{:b} w6:{:b} reserved:{:05b} stuffing_len:{:03b}'.format(prg_mux_rate, w5, w6, reserved, stuffing_len))
+    if False in [w1, w2, w3, w4, w5, w6]: raise Exception('Failure on marker bits witness.')
+    if stuffing_len: raise Exception('Stuffing not handle, abort.')
+    return True
+
+def test_bit(value, offset):
+    '''
+    Return True if offset bit is 1 on value
+
+    :param value: value
+    :type value: int
+    :param offset: bit offset
+    :type offset: int
+    :return: true or false
+    :rtype: boolean
+    '''
+    mask=1<<offset
+    if (value & mask): return True
+    return False
+
 # 0xb9 - 0xff (255+1 because of range)
 streamId=[ bytes.fromhex(f'{n:x}') for n in range(185, 256) ]
 packHeader=[[b'\x00'], [b'\x00'], [b'\x01'], streamId]
 search_index=0
 found=b''
+
 while cliArgs.limit > 0:
-    bytes=vobfile.read(1)
-    n, nofbytes=0, len(bytes)
+    streamId=vobfile.read(4)
+    if streamId == b'\x00\x00\x01\xba':
+        readPackHeader(streamId+vobfile.read(10))
+    elif streamId == b'\x00\x00\x01\xbd':
+        PesPacketLength=vobfile.read(2)
+        readPesPacket(streamId+PesPacketLength+vobfile.read(int.from_bytes(PesPacketLength)))
+    cliArgs.limit-=vobfile.tell()
 
-    while n < nofbytes:
-        n += 1
-        byte = bytes[nofbytes-n:nofbytes-n+1]
-        if ( byte in packHeader[search_index] ):
-            search_index+=1
-            found+=byte
-            if search_index >= len(packHeader):
-                search_index=0
-                print(f'found {found}: {getStreamId(byte)}')
-                if byte == b'\xbd':
-                    print(int.from_bytes(vobfile.read(2)))
-                    e=bin(int.from_bytes(vobfile.read(2)))
-                    print('10:{} scrmbl:{} priority:{} align:{} copyr:{} org:{}'.format(e[2:4], e[4:6], e[6:7], e[7:8], e[8:9], e[9:10]))
-                    print('P|DTS flag:{} escr:{} esrate:{} dsm:{} copyInfo:{} crc:{} extflg:{}'.format(e[10:12], e[12:13], e[13:14], e[14:15], e[15:16], e[16:17], e[17:18]))
-                    pes_data_len=int.from_bytes(vobfile.read(1))
-                    print(bin(pes_data_len), pes_data_len)
-            else:
-                continue
-        else:
-            search_index=0
-            found=b''
-            # print(f'false: {byte} against: {packHeader[search_index]}')
+    # while True:
+    #     # octet=vobfile.read(1)
+    #     # chunk = octet[nofbytes+n:nofbytes+n+1]
+    #     # n += 1
+    #     if ( byte in packHeader[search_index] ):
+    #         search_index+=1
+    #         found+=byte
+    #         if search_index >= len(packHeader):
+    #             search_index=0
+    #             # print(f'found {found}: {getStreamId(byte)}')
+    #             print(f'> {getStreamId(byte)}')
+    #             # stream ID: private stream
+    #             if byte == b'\xbd':
+    #                 PesPayloadLen=int.from_bytes(vobfile.read(2))
 
-            # # found b'G' or 0x47 byte, so now check
-            # # if previous byte is 0x50 or b'P'
-            # n += 1
-            # byte = bytes[nofbytes-n:nofbytes-n+1]
-            # if ( byte == b'P' ):
-            #     # PGS header found, now check if sub segment type is PCS
-            #     pgs_offset = 13
-            #     pgs = bytes[nofbytes-n:nofbytes-n+pgs_offset]
-            #     if ( pgs[10:11] == b'\x16' ):
-            #         pcs_size = int.from_bytes(pgs[11:13])
-            #         pcs_bytes = bytes[nofbytes-n+pgs_offset:nofbytes-n+pgs_offset+pcs_size]
-            #         # PCS pts value is not important here (set to 0)
-            #         pcs = readPCS(pcs_bytes, 0)
-            #         return f'{pcs.video_width} {pcs.video_height} {(pcs.comp_n//2)+1:d}'
-    cliArgs.limit -=1
+    #                 # Extension
+    #                 bits=Bits(vobfile.read(2))
+    #                 print(
+    #                     '>> {} scrmbl:{} priority:{} align:{} copyr:{} org:{}'
+    #                     .format(*bits.harvest([2,2,1,1,1,1]))
+    #                 )
+    #                 PtsDtsFlag, escr, esRate, dsm, copy, crc, extFlag=bits.harvest([2,1,1,1,1,1,1])
+    #                 print(
+    #                     '>> P|DTS flag:{} escr:{} esrate:{} dsm:{} copyInfo:{} crc:{} extflg:{}'
+    #                     .format(PtsDtsFlag, escr, esRate, dsm, copy, crc, extFlag)
+    #                 )
+    #                 PesHeaderDataLen=int.from_bytes(vobfile.read(1))
+    #                 # test if extra PES header present
+    #                 if (PtsDtsFlag == '10'):
+    #                     bits.load(vobfile.read(5))
+    #                     print(bits.harvest([4, 3, 1, 15, 1, 15, 1]))
+    #                 print(f'PES header data length: {PesHeaderDataLen}')
+    #                 print(f'PES payload length: {PesPayloadLen}')
 
-    # header=vobfile.read(10)
-    # systemClock1=header[2:5]
-    # systemClock2=header[6:21]
-    # systemClock3=header[22:37]
-    # srcExtension=header[38:47]
-    # bitRate=header[48:70]
-    # stuffingLength=header[77:80]
-    # print(systemClock1, systemClock2, systemClock3, srcExtension, bitRate, stuffingLength)
+    #                 substreamid=int.from_bytes(vobfile.read(1))
+    #                 print(f'substream id: {substreamid}')
+    #                 PesPayload=vobfile.read(PesPayloadLen-1)
+    #                 print('subtitle packet size: {}'.format(int.from_bytes(PesPayload[:2])))
+    #                 dataPacketSize=int.from_bytes(PesPayload[2:4])
+    #                 print('data packet size: {}'.format(dataPacketSize))
+    #                 print('end sequence position: {}'.format(int.from_bytes(PesPayload[dataPacketSize+2:dataPacketSize+4])))
+    #                 n=0
+    #                 while n <= 32:
+    #                     offset=dataPacketSize+4+n
+    #                     print('ctrl sequence: {} {}'.format(dataPacketSize+n, PesPayload[offset:offset+1]))
+    #                     n+=1
+
+    #                 # print('ff byte ?: {}'.format(int.from_bytes(PesPayload[dataPacketSize+2:dataPacketSize+4])))
+    #                 # print('ctrl sequence: {}'.format(PesPayload[dataPacketSize+7:dataPacketSize+8]))
+    #                 print()
+    #         else:
+    #             continue
+    #     else:
+    #         search_index=0
+    #         found=b''
     # cliArgs.limit -=1
